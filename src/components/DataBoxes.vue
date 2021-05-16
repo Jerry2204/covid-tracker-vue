41054fd9375e20acc4715ede9871c9c192754151
jerry2204 <jerryandrianto22@gmail.com>
2021-05-17 01:27:50 +0700

    finishing covid-tracker

---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="grid md:grid-cols-2 gap-4">
+    <div class="shadow-md bg-blue-100 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New:</span>
+        {{ number_format(stats.NewConfirmed) }}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{ number_format(stats.TotalConfirmed) }}
+      </div>
+    </div>
+    <div class="shadow-md bg-blue-100 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths</h3>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New:</span>
+        {{ number_format(stats.NewDeaths) }}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{ number_format(stats.TotalDeaths) }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DataBoxes",
+  props: ["stats"],
+  methods: {
+    number_format(number) {
+      const formatNumber = new Intl.NumberFormat("id-ID");
+      return formatNumber.format(number);
+    },
+  },
+};
+</script>
